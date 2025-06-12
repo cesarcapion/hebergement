59225eb5379f3b7ac5e6810b2023585d4e9476d4
tony.le-trong <tony.le-trong@epita.fr>
2025-06-12 18:25:04 +0200

    feat(user): implement UserModel.java

--- ping/src/main/java/fr/epita/assistants/ping/data/model/UserModel.java
+++ ping/src/main/java/fr/epita/assistants/ping/data/model/UserModel.java
@@ -1,8 +1,44 @@
 package fr.epita.assistants.ping.data.model;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+
+@Entity
 @Table(name = "users")
 public class UserModel {
-    // TODO
+
+    @Id
+    @GeneratedValue
+    private UUID uuid;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String displayName = "";
+    private String avatar = "";
+    private Boolean isAdmin = false;
+
+    public UUID getUuid() { return uuid; }
+    public void setUuid(UUID uuid) { this.uuid = uuid; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getDisplayName() { return displayName; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+
+    public String getAvatar() { return avatar; }
+    public void setAvatar(String avatar) { this.avatar = avatar; }
+
+    public Boolean getIsAdmin() { return isAdmin; }
+    public void setIsAdmin(Boolean admin) { isAdmin = admin; }
 }
