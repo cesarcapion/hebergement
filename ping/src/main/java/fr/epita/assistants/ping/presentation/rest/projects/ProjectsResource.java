f8e159e4f70eec59b205a75ac5cac8ce11d0958a
clement.pasteau <clement.pasteau@epita.fr>
2025-06-22 18:52:44 +0200

    fix(ProjectResource): added logs

--- ping/src/main/java/fr/epita/assistants/ping/presentation/rest/projects/ProjectsResource.java
+++ ping/src/main/java/fr/epita/assistants/ping/presentation/rest/projects/ProjectsResource.java
@@ -16,10 +16,7 @@ import fr.epita.assistants.ping.data.model.UserModel;
 import fr.epita.assistants.ping.domain.service.ProjectMembersService;
 import fr.epita.assistants.ping.domain.service.ProjectService;
 import fr.epita.assistants.ping.domain.service.UserService;
-import fr.epita.assistants.ping.utils.ErrorInfo;
-import fr.epita.assistants.ping.utils.Feature;
-import fr.epita.assistants.ping.utils.RequestVerifyer;
-import fr.epita.assistants.ping.utils.UserStatus;
+import fr.epita.assistants.ping.utils.*;
 import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
@@ -29,6 +26,8 @@ import jakarta.ws.rs.core.Response;
 
 @Path("/api/projects")
 public class ProjectsResource {
+    @Inject
+    Logger logger;
     @Inject
     ProjectService projectService;
     @Inject
@@ -45,7 +44,10 @@ public class ProjectsResource {
     @RolesAllowed({"user", "admin"})
     @Produces(MediaType.APPLICATION_JSON)
     public Response getProjects(@DefaultValue("false") @QueryParam("onlyOwned") boolean onlyOwned) {
+        logger.logInfo(identity.getPrincipal().getName() + " requested to get all the projects where he is " + (onlyOwned ? "an owner" : "a member"));
+
         ArrayList<ProjectResponse> projectResponse = projectService.buildGetProjectsResponse(identity.getPrincipal().getName(), onlyOwned);
+        logger.logSuccess("The operation was successful");
         return Response.status(200).entity(projectResponse).build();
     }
 
@@ -55,11 +57,15 @@ public class ProjectsResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createProject(NewProjectRequest newProjectRequest) {
-//        if (newProjectRequest == null || newProjectRequest.name == null || newProjectRequest.name.isEmpty()){
+        // FIXME params what to put when request is null?
+        logger.logInfo(identity.getPrincipal().getName() + " requested to create a new project");
         if (RequestVerifyer.isInvalid(newProjectRequest)) {
+            logger.logError("Error 400: The project name is invalid");
+
             return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorInfo("The project name is invalid")).build();
         } else {
             UserModel user = userService.get(UUID.fromString(identity.getPrincipal().getName()));
+            logger.logSuccess("The operation was successful");
             return Response.status(Response.Status.OK).entity(projectService.buildCreateProjectResponse(newProjectRequest.name, user)).build();
         }
     }
@@ -70,7 +76,8 @@ public class ProjectsResource {
     @RolesAllowed("admin")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllProjects() {
-        System.out.println(">>> getAllProjects() called");
+        logger.logInfo(identity.getPrincipal().getName() + " requested to get all projects");
+        logger.logSuccess("The operation was successful");
         return Response.status(200).entity(projectService.buildGetAllProjectsResponse()).build();
     }
 
@@ -80,7 +87,10 @@ public class ProjectsResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateProject(@PathParam("id") UUID projectId, UpdateProjectRequest updateProjectRequest) {
+        // FIXME loginfo request params
+        logger.logInfo(identity.getPrincipal().getName() + " requested to update the project with id " + projectId);
         if (RequestVerifyer.isInvalid(updateProjectRequest)) {
+            logger.logError("Error 400: Bad update project request, fields are null or the request is null");
             return Response.status(Response.Status.BAD_REQUEST).
                     entity(new ErrorInfo("Bad update project request, fields are null or the request is null"))
                     .build();
@@ -90,25 +100,30 @@ public class ProjectsResource {
         UserStatus userStatus = projectService.getUserStatus(currentUser.getId(), projectId, currentUser.getIsAdmin());
 
         if (userStatus == UserStatus.NOT_A_MEMBER) {
+            logger.logError("Error 403: Not allowed to update this project as you are not member of it");
             return Response.status(Response.Status.FORBIDDEN)
                     .entity(new ErrorInfo("Not allowed to update this project as you are not member of it"))
                     .build();
         }
 
         if (userStatus == UserStatus.MEMBER) {
+            logger.logError("Error 403: Not allowed to update this project since you are only member, not owner or admin");
             return Response.status(Response.Status.FORBIDDEN)
                     .entity(new ErrorInfo("Not allowed to update this project since you are only member, not owner or admin"))
                     .build();
         }
 
         if (userStatus == UserStatus.ERROR) {
+            logger.logError("Error 404: The project does not exist");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("The project does not exist")).build();
         }
         UUID newOwnerId = updateProjectRequest.newOwnerId != null ? UUID.fromString(updateProjectRequest.newOwnerId) : null;
         ProjectModel updatedProject = projectService.UpdateProject(projectId, newOwnerId, updateProjectRequest.name);
         if (updatedProject == null) {
+            logger.logError("Error 404: The new owner is not a member of this project");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("The new owner is not a member of this project")).build();
         }
+        logger.logSuccess("The operation was successful");
         return Response.status(Response.Status.OK).entity(projectService.buildGetProjectResponse(updatedProject)).build();
     }
 
@@ -117,16 +132,20 @@ public class ProjectsResource {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getProject(@PathParam("id") UUID projectId) {
+        logger.logInfo(identity.getPrincipal().getName() + " requested to get the project with id " + projectId);
         UserModel currentUser = userService.get(UUID.fromString(identity.getPrincipal().getName()));
 
         UserStatus userStatus = projectService.getUserStatus(currentUser.getId(), projectId, currentUser.getIsAdmin());
         if (userStatus == UserStatus.NOT_A_MEMBER && !currentUser.getIsAdmin()) {
+            logger.logError("Error 403: The new owner is not a member of this project");
             return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not allowed to get this project as you are not member")).build();
         }
         if (userStatus == UserStatus.ERROR) {
+            logger.logError("Error 404: The project does not exist");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("The project does not exist")).build();
         }
         // else the user is either MEMBER, OWNER or ADMIN so he can access the project
+        logger.logSuccess("The operation was successful");
         return Response.status(Response.Status.OK).entity(projectService.buildGetProjectResponseWithId(projectId)).build();
     }
 
@@ -134,24 +153,28 @@ public class ProjectsResource {
     @RolesAllowed({"admin", "user"})
     @Path("/{id}")
     public Response deleteProject(@PathParam("id") UUID projectId) {
-
+        logger.logInfo(identity.getPrincipal().getName() + " requested to delete the project with id " + projectId);
         UserModel currentUser = userService.get(UUID.fromString(identity.getPrincipal().getName()));
 
         UserStatus userStatus = projectService.getUserStatus(currentUser.getId(), projectId, currentUser.getIsAdmin());
         if (!currentUser.getIsAdmin()) {
             if (userStatus == UserStatus.NOT_A_MEMBER) {
+                logger.logError("Error 403: Not allowed to delete this project as you are not member");
                 return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not allowed to delete this project as you are not member")).build();
             }
             if (userStatus == UserStatus.MEMBER) {
+                logger.logError("Error 403: Not allowed to delete this project as you are only a member");
                 return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not allowed to delete this project as you are only a member")).build();
             }
         }
         if (userStatus == UserStatus.ERROR) {
+            logger.logError("Error 404: The project does not exist");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("The project does not exist")).build();
         }
         // else the user is either OWNER or ADMIN so he can delete the project
         projectService.deleteProjectById(projectId);
         projectMembersService.deleteAllMembers(projectId);
+        logger.logSuccess("The operation was successful");
         // when deleting the project make sure to remove all its members as well from the ProjectMembers database
         return Response.status(Response.Status.NO_CONTENT).build();
     }
@@ -161,20 +184,26 @@ public class ProjectsResource {
     @RolesAllowed({"user", "admin"})
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addUserToProject(@PathParam("id") UUID projectId, UserProjectRequest userProjectRequest) {
+        // FIXME loginfo request params
+        logger.logInfo(identity.getPrincipal().getName() + " requested to add a user to the project with id " + projectId);
         if (RequestVerifyer.isInvalid(userProjectRequest)) {
+            logger.logError("Error 400: Null request, null userId, empty userId or invalid uuid for userId");
             return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorInfo("Null request, null userId, empty userId or invalid uuid for userId")).build();
         }
 
         UserModel currentUser = userService.get(UUID.fromString(identity.getPrincipal().getName()));
         boolean userExists = userService.get(UUID.fromString(userProjectRequest.userId)) != null;
         if (!userExists) {
+            logger.logError("Error 404: User to add not found");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("User to add not found")).build();
         }
         UserStatus userStatus = projectService.getUserStatus(currentUser.getId(), projectId, currentUser.getIsAdmin());
         if (userStatus == UserStatus.ERROR) {
+            logger.logError("Error 404: Project not found");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("Project not found")).build();
         }
         if (userStatus == UserStatus.NOT_A_MEMBER && !currentUser.getIsAdmin()) {
+            logger.logError("Error 403: Not a member of the project nor an admin, cannot add a user to this project");
             return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not a member of the project nor an admin, cannot add a user to this project")).build();
         }
         // now the user is either member, owner or admin so he can add a new user to the project
@@ -183,9 +212,11 @@ public class ProjectsResource {
                         UUID.fromString(userProjectRequest.userId),
                         projectId);
         if (!added) {
+            logger.logError("Error 409: The user is already member of the project");
             return Response.status(Response.Status.CONFLICT).entity(new ErrorInfo("The user is already member of the project")).build();
         }
 
+        logger.logSuccess("The operation was successful");
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
@@ -193,24 +224,30 @@ public class ProjectsResource {
     @Path("/{id}/remove-user")
     @RolesAllowed({"user", "admin"})
     public Response removeUserFromProject(@PathParam("id") UUID projectId, UserProjectRequest userProjectRequest) {
-        //        if (userProjectRequest == null || userProjectRequest.userId == null || userProjectRequest.userId.isEmpty()) {
+        // FIXME loginfo request params
+        logger.logInfo(identity.getPrincipal().getName() + " requested to remove a user from the project with id " + projectId);
         if (RequestVerifyer.isInvalid(userProjectRequest)) {
+            logger.logError("Error 400: Null request, null userId, empty userId or invalid uuid for userId");
             return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorInfo("Null request, null userId, empty userId or invalid uuid for userId")).build();
         }
         UserModel targetedUser = userService.get(UUID.fromString(userProjectRequest.userId));
         boolean userExist = targetedUser != null;
         if (!userExist) {
+            logger.logError("Error 404: User to remove not found");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("User to remove not found")).build();
         }
         UserStatus userStatus = projectService.getUserStatus(UUID.fromString(identity.getPrincipal().getName()), projectId, false);
 
         if (userStatus == UserStatus.ERROR) {
+            logger.logError("Error 404: Project not found");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("Project not found")).build();
         }
         if (userStatus == UserStatus.NOT_A_MEMBER) {
+            logger.logError("Error 403: Not allowed to remove this user from the project as you are not member");
             return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not allowed to remove this user from the project as you are not member")).build();
         }
         if (userStatus == UserStatus.MEMBER) {
+            logger.logError("Error 403: Not allowed to remove this user as you are only a member, not an admin nor the owner");
             return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not allowed to remove this user as you are only a member, not an admin nor the owner")).build();
         }
 
@@ -218,14 +255,16 @@ public class ProjectsResource {
         UserStatus userToRemoveStatus = projectService.getUserStatus(targetedUser.getId(), projectId, targetedUser.getIsAdmin());
 
         if (userToRemoveStatus == UserStatus.OWNER) {
+            logger.logError("Error 403: Not allowed to remove this user because he currently owns the project");
             return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not allowed to remove this user because he currently owns the project")).build();
         }
         if (userToRemoveStatus == UserStatus.NOT_A_MEMBER) {
+            logger.logError("Error 404: the user to remove is not member of this project");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("the user to remove is not member of this project")).build();
         }
         // boolean returned from the function below useless now as the user is a member of the project
         projectMembersService.deleteUserFromProject(UUID.fromString(userProjectRequest.userId), projectId);
-
+        logger.logSuccess("The operation was successful");
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
@@ -233,27 +272,34 @@ public class ProjectsResource {
     @Path("/{id}/exec")
     @RolesAllowed({"admin", "user"})
     public Response execFeatureFromProject(@PathParam("id") UUID projectId, ExecFeatureRequest execFeatureRequest) {
+        // FIXME loginfo request params
+        logger.logInfo(identity.getPrincipal().getName() + " requested to add a user to the project with id " + projectId);
         boolean isAdmin = userService.get(UUID.fromString(identity.getPrincipal().getName())).getIsAdmin();
 
         if (RequestVerifyer.isInvalid(execFeatureRequest)) {
+            logger.logError("Error 400: Invalid request");
             return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorInfo("Invalid request")).build();
         }
 
         UserStatus userStatus = projectService.getUserStatus(UUID.fromString(identity.getPrincipal().getName()), projectId, false);
         if (userStatus == UserStatus.ERROR) {
+            logger.logError("Error 404: Project not found");
             return Response.status(Response.Status.NOT_FOUND).entity(new ErrorInfo("Project not found")).build();
         }
 
         if (userStatus == UserStatus.NOT_A_MEMBER && !isAdmin) {
+            logger.logError("Error 403: Not member of this project, cannot exec feature");
             return Response.status(Response.Status.FORBIDDEN).entity(new ErrorInfo("Not member of this project, cannot exec feature")).build();
         }
         // member, owner or admin
         boolean succeeded = projectService.execFeature(projectId, Feature.valueOfLabel(execFeatureRequest.feature)
                 , execFeatureRequest.command, execFeatureRequest.params);
         if (!succeeded) {
+            logger.logError("Error 500: Could not execute feature");
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorInfo("Could not execute feature")).build();
         }
 
+        logger.logSuccess("The operation was successful");
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
