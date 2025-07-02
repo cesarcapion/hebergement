f1ecdce7dcfe12513c43d14ccd4a7365fbdaa0ad
remy.mazri <remy.mazri@epita.fr>
2025-07-02 14:30:22 +0200

    feat: check si la personne est admin

---
+++ ping/front/PING/src/AdminRoute.tsx
@@ -0,0 +1,29 @@
+import type {JSX} from 'react';
+import { Navigate } from 'react-router-dom';
+const getUserGroupFromToken = (): string | null => {
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+
+    try {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        console.log("payload: " + payload.groups);
+        return payload.groups || null;
+    } catch {
+        console.log("bon la g juste pas reussi a parser")
+        return null;
+    }
+};
+
+export const AdminRoute = ({ children }: { children: JSX.Element }) => {
+    const token = localStorage.getItem('token');
+    const group = getUserGroupFromToken();
+    if (!token) return <Navigate to="/login" replace />;
+    console.log("LE GROUPE EST \'" + group + "\'");
+    if (group === null || group.toString() !== "admin") {
+        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
+        return <Navigate to="/" replace/>;
+    }
+
+    return children;
+};
+export default AdminRoute;
---
+++ ping/src/main/java/fr/epita/assistants/ping/api/request/StatRequest.java
@@ -0,0 +1,4 @@
+package fr.epita.assistants.ping.api.request;
+
+public class StatRequest {
+}
