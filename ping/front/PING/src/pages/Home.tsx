b484e2265cee1d728f6830ab50401d2ad9922149
clement.pasteau <clement.pasteau@epita.fr>
2025-07-03 14:39:37 +0200

    fix(HomePage): Maxanox fix element position

--- ping/front/PING/src/pages/Home.tsx
+++ ping/front/PING/src/pages/Home.tsx
@@ -27,25 +27,26 @@ const Home = () => {
     return (
     <>
       <Link className="profile-button" to="/profile" title="My Profile">👤</Link>
-
-      <div className="header-block">
-        <div className="text-block">
-          <div className="title">Tick-E Taka:</div>
-          <div className="subtitle">
-            <u>Every question passes,</u><br />
-            <u>Every answer scores.</u>
+      <div className="flex flex-col items-center w-screen">
+        <div className="header-block">
+          <div className="text-block">
+            <div className="title">Tick-E Taka:</div>
+            <div className="subtitle">
+              <u>Every question passes,</u><br />
+              <u>Every answer scores.</u>
+            </div>
           </div>
+          <img src="/White-Logo-without-bg.png" alt="Logo" className="logo" />
         </div>
-        <img src="/White-Logo-without-bg.png" alt="Logo" className="logo" />
-      </div>
 
-      <div className="button-group">
-          <Link to="/qa">
-              <button className="btn">Q&amp;A</button>
-          </Link>
-          <Link to="/my-tickets">
-              <button className="btn">My Tickets</button>
-          </Link>
+        <div className="button-group">
+            <Link to="/qa">
+                <button className="btn">Q&amp;A</button>
+            </Link>
+            <Link to="/my-tickets">
+                <button className="btn">My Tickets</button>
+            </Link>
+        </div>
       </div>
     </>
   );
