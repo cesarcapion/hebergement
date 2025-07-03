2788c8ddd21144694ed24d123416bc92bc984e28
cesar capion <cesarcapion@gmail.com>
2025-07-03 23:20:00 +0200

    fix: changing base url

--- ping/front/PING/src/api/login.tsx
+++ ping/front/PING/src/api/login.tsx
@@ -15,11 +15,11 @@ export const login = async (email: string, password: string): Promise<string> =>
       const group = getUserGroupFromToken();
       if (group?.toString() == "user")
       {
-        window.location.href = `${import.meta.env.VITE_SERVER_URL}`;
+        window.location.href = `${import.meta.env.VITE_BASE_URL}`;
       }
       else
       {
-        window.location.href = `${import.meta.env.VITE_SERVER_URL}admin`;
+        window.location.href = `${import.meta.env.VITE_BASE_URL}admin`;
       }
 
       return ''
